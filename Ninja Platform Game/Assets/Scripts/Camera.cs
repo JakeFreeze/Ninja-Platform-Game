42215aa2c2f5bbe1e8a7b598946230a2c4f8aa6e
Max <max.edman@yahoo.com>
2017-03-07 17:07:01 -0600

    New Unity Project added
    Game no longer works so all development will be done on Ninja Platform
    Game

---
+++ Ninja Platform Game/Assets/Scripts/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Camera : MonoBehaviour {
+
+	public Rigidbody2D target;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - target.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (offset.x > 1) {
+			Start ();
+		} else {
+
+			transform.position = target.transform.position + offset;
+		}
+		
+	}
+}
---
+++ Ninja Platform Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public bool OnHead;
+	public float hitDistance;
+	public LayerMask layer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (OnHead)
+			Destroy (gameObject);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		if (Physics2D.Raycast (transform.position, transform.up, hitDistance, layer))
+			OnHead = true;
+		else
+			OnHead = false;
+
+	}
+}
