93ef911955c2f4e7940ef30bd57ade39c38f3391
Max <max.edman@yahoo.com>
2017-03-07 15:09:51 -0600

    Game Added
    Unity Project added to repository

---
+++ Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string level)
+    {
+
+        //Debug.Log("Attempting to load " + level);
+
+        Application.LoadLevel(level);
+
+    }
+
+    public void quitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
